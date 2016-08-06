c8de8463c7c108865f0f29f2449b5dc94af59f64
Jeremy Gunter <gunterja@gmail.com>
2016-08-05 21:51:49 -0700

    Draw assocaiations between blogs, users, and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_one :blog, dependent: :destroy
+  has_many :posts, through: :blogs, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806035305) do
+ActiveRecord::Schema.define(version: 20160806043514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20160806035305) do
     t.integer  "owner"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["blog_id"], name: "index_posts_on_blog_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -33,4 +43,5 @@ ActiveRecord::Schema.define(version: 20160806035305) do
   end
 
   add_foreign_key "blogs", "users", column: "owner", on_delete: :cascade
+  add_foreign_key "posts", "blogs"
 end
