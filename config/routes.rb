398bdfb234757a1b029cc6ef44b2d438410af6c6
Jeremy Gunter <gunterja@gmail.com>
2016-08-05 22:26:51 -0700

    Set routing to match requirements from exercise desc

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'forum#index'
 
+  resources :blogs do
+    resources :posts
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
