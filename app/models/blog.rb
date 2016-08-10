6bd048e3a09d2bb27ee437c042b2f8526a43cc63
Jeremy Gunter <gunterja@gmail.com>
2016-08-10 16:45:12 -0700

    Update Blog model to allow calling its owners name

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,6 @@ class Blog < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug, use: :slugged
 
-  belongs_to :user
+  belongs_to :user, :class_name => User, :foreign_key => :owner
   has_many :posts
 end
