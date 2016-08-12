773730d19d55174ae3a056a1965cdf914c93e1c7
Jeremy Gunter <gunterja@gmail.com>
2016-08-11 18:46:22 -0700

    Update Blog associations after changing owner column to user_id

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,6 @@ class Blog < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug, use: :slugged
 
-  belongs_to :user, :class_name => User, :foreign_key => :owner
+  belongs_to :user, :class_name => User, :foreign_key => :user_id
   has_many :posts
 end
